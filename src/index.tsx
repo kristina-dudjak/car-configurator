e1e374c99be9044ca688adfa364199398b10453b
Kristina Dudjak <dudjakkristina@gmail.com>
2022-07-21 17:17:22 +0200

    Install normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'App';
 import './index.css';
+require('normalize-css');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
