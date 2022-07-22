733a25e46ad70ab6f1578606e4533357cd6ff599
Kristina Dudjak <dudjakkristina@gmail.com>
2022-07-22 17:41:44 +0200

    Add dropdown menu

--- src/shared/components/header/Header.styles.ts
+++ src/shared/components/header/Header.styles.ts
@@ -4,12 +4,42 @@ const container = css`
   position: sticky;
   top: 0;
   z-index: 2;
+`;
+
+const container__header = css`
   background-color: #2e2e38;
   color: #fff;
   display: flex;
-  flex-shrink: 0;
   justify-content: space-between;
   padding: 20px 40px;
 `;
 
-export default { container };
+function container__menu(visible: boolean) {
+  return css({
+    position: 'absolute',
+    right: '0',
+    display: visible ? 'flex' : 'none',
+    flexDirection: 'column',
+    width: '218px',
+    backgroundColor: '#ffffff',
+    boxShadow: '0px 10px 40px rgba(0, 0, 0, 0.06)',
+  });
+}
+
+const container__menu__text = css`
+  padding: 16px 24px;
+  font-size: 14px;
+  color: #2e2e38;
+  border: 1px solid #f1f1f4;
+  cursor: pointer;
+  &:hover {
+    color: #3f3fe4;
+  }
+`;
+
+export default {
+  container,
+  container__header,
+  container__menu,
+  container__menu__text,
+};
