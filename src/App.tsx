c933721e0e7467039d6d0d44805781a0532ab825
Kristina Dudjak <dudjakkristina@gmail.com>
2022-07-28 00:16:32 +0200

    Add registration and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from 'shared';
-import { Home } from 'views';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout, userAtoms } from 'shared';
+import { Home, Login, Register } from 'views';
+import { useRecoilValue } from 'recoil';
 
 export function App() {
+  const uid = useRecoilValue(userAtoms.userUid);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={uid != '' ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/register"
+          element={uid != '' ? <Navigate to="/" /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={uid != '' ? <Navigate to="/" /> : <Login />}
+        />
       </Route>
     </Routes>
   );
