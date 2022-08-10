268c760868068b3763348433e011f8cde95c957a
Kristina Dudjak <dudjakkristina@gmail.com>
2022-08-10 09:56:20 +0200

    Add car and configuration atoms

---
+++ src/modules/configuration/state/atoms.ts
@@ -0,0 +1,33 @@
+import { Timestamp } from 'firebase/firestore';
+import { Car, Configuration } from 'modules';
+import { atom } from 'recoil';
+
+const car = atom<Car>({
+  key: 'car',
+  default: {
+    name: '',
+    year: 0,
+    url: '',
+    price: 0,
+    colors: [],
+    wheels: [],
+    interiors: [],
+  },
+});
+
+const configuration = atom<Configuration>({
+  key: 'configuration',
+  default: {
+    modelName: '',
+    color: { id: '', name: '', thumbnail: '', price: 0 },
+    wheel: { id: '', name: '', thumbnail: '', price: 0 },
+    interior: { id: '', name: '', thumbnail: '', price: 0 },
+    price: 0,
+    creationDate: Timestamp.fromMillis(0),
+  },
+});
+
+export const carAtoms = {
+  car,
+  configuration,
+};
